a32e9ac2a73222a99b3a2b2ad1345cae5a22e44b
shivam-bhargav <bhargav.shivam733@gmail.com>
2025-09-12 19:28:54 +0530

    All setup configured and minor bug in index.js file fixed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
-const app = express()
+const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -10,4 +10,12 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+// console.log(userRouter);
+
+//routes declaration    
+app.use("/api/v1/users",userRouter)
+
 export {app}
--- src/db/index.db.js
+++ src/db/index.db.js
@@ -3,6 +3,7 @@ import { DB_NAME } from "../contants.js";
 
 const connectDB = async (error) =>{
     try {
+        // console.log("LELELEL ",DB_NAME);
         const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
